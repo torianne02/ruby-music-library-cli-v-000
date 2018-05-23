bf8321d93ef39336bb44d882eddaf4abac4ad8df
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-23 15:08:00 +0000

    automatically backed up by learn

---
+++ lib/music_library_controller.rb
@@ -0,0 +1,13 @@
+class MusicLibraryController
+  def initialize(path = "./db/mp3s")
+    MusicImporter.new(path).import
+  end
+
+  def call
+    puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
+  end
+
+end
